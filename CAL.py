67898ef0be457e0198ca6446b2c1cb429b67b732
tima <timamironuc@gmail.com>
2021-06-02 22:33:36 +0300

    fix errors and delate gabarge and make beautifull

--- CAL.py
+++ CAL.py
@@ -4,33 +4,43 @@ MathProblem =  (input("Please enter any math problem"))
 Numbers = []
 Operators = []
 
+PieseNumber=None
 
 for i in MathProblem:
-    if (i == '-' or '+' or '*' or '/'):
-        Operators.add(i)
-        Numbers.add(Number)
-        Number=None
-        
+    if (i == '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9' or i == '0'):
+        if (PieseNumber == None):
+            PieseNumber = i
+        else:
+            PieseNumber = PieseNumber + i
+            
     else:
-        Number= int(str(Number)+i)
+        Operators.append(i)
+        Numbers.append(int(PieseNumber))     
+        PieseNumber = None
 
-a=1
-Result=Numbers[0]
-while(a!=Operators.len):   
+Numbers.append(int(PieseNumber))
+
+a = 1
+
+Result = Numbers[0]
+
+
+while(a <= len(Operators)):   
     
     if (Operators[a-1] == "+"):
-        Result = Result+Numbers[a]
+        Result = Result + Numbers[a]
+
 
     elif (Operators[a-1] == "-"):
-        Result = Result-Numbers[a]
+        Result = Result - Numbers[a]
 
     elif (Operators[a-1] == "*"):
-        Result = Result*Numbers[a]
+        Result = Result * Numbers[a]
 
     elif(Operators[a-1] == "/"):
-        Result = Result/Numbers[a]
+        Result = Result / Numbers[a]
         
-    a=a+1
+    a = a + 1
 
 
-print('Result is ' + str( Result)  )
+print('Result is ' + str( Result))
