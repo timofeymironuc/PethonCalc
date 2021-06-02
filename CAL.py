287e3570489836cea47d6725560d76d9af5d1ac7
tima <timamironuc@gmail.com>
2021-06-02 17:10:49 +0300

    Beatufull

--- CAL.py
+++ CAL.py
@@ -1,27 +1,25 @@
 print("This is calculator program")
 
+FirstNumber = float (input("Please enter first number"))
+SecondNumber = float (input("Please enter second number"))
 
-
-FirstNumber=int(input("Please enter first number"))
-SecondNumber=int(input("Please enter second number"))
-
-Key=input("Please enter operator")
+Key = input ("Please enter operator")
 
 
 
-if (Key=="+"):
-    Result=FirstNumber+SecondNumber
+if (Key == "+"):
+    Result = FirstNumber + SecondNumber
 
-elif (Key=="-"):
-    Result=FirstNumber-SecondNumber
+elif (Key == "-"):
+    Result = FirstNumber - SecondNumber
 
-elif (Key=="*"):
-    Result=FirstNumber*SecondNumber
+elif (Key == "*"):
+    Result = FirstNumber * SecondNumber
 
-elif(Key=="/"):
-    Result=FirstNumber/SecondNumber
+elif(Key == "/"):
+    Result = FirstNumber/SecondNumber
 
 else:
     print('You entered an incorrect symbol.')
 
-print('Result is ' + str( Result) + "." )
+print('Result is ' + str( Result)  )
