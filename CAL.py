0d64e0a3c353b2e521a59c2987548d58b9d224c9
tima <timamironuc@gmail.com>
2021-06-02 16:36:33 +0300

    without print

---
+++ CAL.py
@@ -0,0 +1,18 @@
+FirstNumber=int(input("First number"))
+SecondNumber=int(input("Second number"))
+
+Key=input("Key")
+
+
+
+if (Key=="+"):
+    Result=FirstNumber+SecondNumber
+
+elif (Key=="-"):
+    Result=FirstNumber-SecondNumber
+
+elif (Key=="*"):
+    Result=FirstNumber*SecondNumber
+
+else:
+    Result=FirstNumber/SecondNumber
